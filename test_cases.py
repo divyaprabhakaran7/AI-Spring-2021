c6f34c944f262d0640183d97c0d3ad37127e55dd
divyaprabhakaran7 <39270710+divyaprabhakaran7@users.noreply.github.com>
2021-03-14 12:25:55 -0500

    Updated test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -1,21 +1,21 @@
 #Test cases for the scheduler to run
 def main():
-my_country_scheduler('Atlantis', 'data/resource_data.xslx', 'data/test_case_1.xslx', 'data/output_data.xslx',
+my_country_scheduler('Atlantis', 'data/resource_data.xslx', 'data/test_case_1.xslx', 'data/output_data1-1.xslx',
                      5, 3, 10)
 
-my_country_scheduler('Atlantis', 'data/resource_data.xslx', 'data/test_case_2.xslx', 'data/output_data.xslx',
+my_country_scheduler('Atlantis', 'data/resource_data.xslx', 'data/test_case_2.xslx', 'data/output_data1-2.xslx',
                      7, 4, 8)
 
-my_country_scheduler('Atlantis', 'data/resource_data.xslx', 'data/test_case_2.xslx', 'data/output_data.xslx',
+my_country_scheduler('Atlantis', 'data/resource_data.xslx', 'data/test_case_2.xslx', 'data/output_data2-1.xslx',
                      5, 3, 10)
 
-my_country_scheduler('Atlantis', 'data/resource_data.xslx', 'data/test_case_3.xslx', 'data/output_data.xslx',
+my_country_scheduler('Atlantis', 'data/resource_data.xslx', 'data/test_case_3.xslx', 'data/output_data2-2.xslx',
                      7, 4, 8)
 
-my_country_scheduler('Atlantis', 'data/resource_data.xslx', 'data/test_case_3.xslx', 'data/output_data.xslx',
+my_country_scheduler('Atlantis', 'data/resource_data.xslx', 'data/test_case_3.xslx', 'data/output_data3-1.xslx',
                      5, 3, 10)
 
-my_country_scheduler('Atlantis', 'data/resource_data.xslx', 'data/test_case_4.xslx', 'data/output_data.xslx',
+my_country_scheduler('Atlantis', 'data/resource_data.xslx', 'data/test_case_4.xslx', 'data/output_data3-2.xslx',
                      7, 4, 8)
 
 if __name__ == '__main__':
